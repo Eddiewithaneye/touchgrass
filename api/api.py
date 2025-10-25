37e07fe07c201d15b9444333dc0c449f4a5faed8
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-10-25 11:06:39 -0400

    working api

---
+++ api/api.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from werkzeug.datastructures import FileStorage
+from ImageIdentifier import ImageIdentifier
+from werkzeug.utils import secure_filename
+import time
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "./uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/analyze", methods=["POST"])
+def analyze_image():
+	if "file" not in request.files:
+		return jsonify({"error": "No file part"}), 400
+
+	file: FileStorage = request.files["file"]
+	if file.filename == "":
+		return jsonify({"error": "No selected file"}), 400
+	
+	file_content: bytes = file.stream.read()
+	identifier = ImageIdentifier()
+	result = identifier.checkImageFile(file_content, ["city", "skyscraper"])
+
+	return jsonify({"message": f"{result}"}), 200
+
+if __name__ == "__main__":
+    # debug=True → equivalent to --debug
+    # host="0.0.0.0" → equivalent to -h 0.0.0.0
+    # port=5000 → default Flask port
+	app.run(debug=True, host="0.0.0.0", port=5000)
