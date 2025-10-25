68ea35f2a61524ee7357f1b0fc3c635a9ccf678f
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-10-25 16:14:32 -0400

    got everything working

--- api/analyze_image.py
+++ api/analyze_image.py
@@ -2,14 +2,9 @@ from flask import Flask, request, jsonify
 from werkzeug.datastructures import FileStorage
 from ImageIdentifier import ImageIdentifier
 from werkzeug.utils import secure_filename
-import time
 import os
 from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-UPLOAD_FOLDER = "./uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+from app import app
 
 @app.route("/analyze", methods=["POST"])
 def analyze_image():
@@ -27,9 +22,3 @@ def analyze_image():
 		return jsonify({"message": f"{result}", "challenge_success": True}), 200
 	else:
 		return jsonify ({"message": "no image found", "challenge_success": False}), 200
-
-if __name__ == "__main__":
-    # debug=True → equivalent to --debug
-    # host="0.0.0.0" → equivalent to -h 0.0.0.0
-    # port=5000 → default Flask port
-	app.run(debug=True, host="0.0.0.0", port=5000)
